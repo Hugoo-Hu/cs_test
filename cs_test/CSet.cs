e232769140107698698e2ff59b9732c4fd88a8b3
Hugoo <uquvfhpdeg@gmail.com>
2013-10-24 18:49:53 +0800

    cs test commit

---
+++ cs_test/CSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cs_test
+{
+    public class CSet<T>
+    {
+        private List<T> m_setlist;
+
+        public CSet()
+        {
+            m_setlist = new List<T>();
+        }
+
+        public void add(T item)
+        {
+            if (!m_setlist.Contains(item))
+            {
+                m_setlist.Add(item);
+                m_setlist.Sort();
+            }
+        }
+
+        public T this[int index]
+        {
+            get { return m_setlist[index]; }
+            set { m_setlist[index] = value; }
+        }
+
+        public void remove(T item)
+        { 
+            }
+    
+    }
+}
